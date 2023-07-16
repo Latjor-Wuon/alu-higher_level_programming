d789d95631fef0f57bd285d6f9424b145cd9992e
Latjor-Wuon <l.dak@alustudent.com>
2023-07-16 10:22:49 -0700

    3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if not my_list:
-        return None
-    my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+
+
+def common_elements(set_1, set_2):
+    """
+    A function that returns a set of
+    common elements in two sets
+    """
+    return(set_1 & set_2)
