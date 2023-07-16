75bda53dea298f31dd3dd5e01f59f7de5ff65e29
Latjor-Wuon <l.dak@alustudent.com>
2023-07-16 10:18:05 -0700

    2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-
-    my_list[idx] = element
-    return my_list
+def uniq_add(my_list=[]):
+    uniq_list = []
+    for i in my_list:
+        if i not in uniq_list:
+            uniq_list.append(i)
+    return sum(uniq_list)
