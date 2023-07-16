455ed2eb8671dbef8647641c37a04bd854f7e8bd
Latjor-Wuon <l.dak@alustudent.com>
2023-07-16 10:20:16 -0700

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
-        return None
-
-    return my_list[idx]
+def search_replace(my_list, search, replace):
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
+    return new_list
