4c63e41a43fea5044ed42a19b19ff02adf22d43a
Latjor-Wuon <l.dak@alustudent.com>
2023-07-26 07:46:35 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''Defining a new class.'''
+
+
+class Square:
+    '''Representing a new Square.'''
+
+    def __init__(self, size):
+        '''Initializing a new class.'''
+
+        self.__size = size
