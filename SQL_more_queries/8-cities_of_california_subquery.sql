a3b61df07857c13ef35ee1b8091c44db7d7c28bb
Latjor-Wuon <l.dak@alustudent.com>
2023-08-04 04:06:26 -0700

    8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,7 @@
--- Displays the number of records with id = 89 in the table first_table in my MySQL server.
-SELECT COUNT(*)
-FROM `first_table`
-WHERE `id` = 89;
+-- lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- The states table contains only one record where name = California
+-- Results must be sorted in ascending order by cities.id
+-- You are not allowed to use the JOIN keyword
+-- The database name will be passed as an argument of the mysql command
+
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
