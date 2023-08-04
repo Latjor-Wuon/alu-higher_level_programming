f7e5b930126edcb01bfa3530ef33aca3ef4108e6
Latjor-Wuon <l.dak@alustudent.com>
2023-08-04 04:00:13 -0700

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,2 +1,10 @@
--- Lists all rows of the table first_table in my MySQL server.
-SELECT * FROM `first_table`;
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- states description:
+-- 	  id INT unique, auto generated, can’t be null and is a primary key
+--	  name VARCHAR(256) can’t be null
+-- If the database hbtn_0d_usa already exists, your script should not fail
+-- If the table states already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL);
