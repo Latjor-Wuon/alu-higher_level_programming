80617756665a1aee50d28aa3c46fb397b4afef59
Latjor-Wuon <l.dak@alustudent.com>
2023-08-17 11:43:49 -0700

    model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""
-Prints all City objects from the database hbtn_0e_14_usa
-"""
-import sys
-from model_state import Base, State
-from model_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+""" Define State model """
+from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
 
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+class City(Base):
+    """ Define a class State to be linked to db table """
+    __tablename__ = 'cities'
 
-    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+    id = Column(Integer, nullable=False,
+                autoincrement=True, unique=True,
+                primary_key=True)
 
-    for state, city in st_cty:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    name = Column(String(128), nullable=False)
+
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
