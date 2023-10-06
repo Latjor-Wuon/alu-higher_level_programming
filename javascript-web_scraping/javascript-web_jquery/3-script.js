7ff635bedc233de69295c5b09644a86a9041a723
Latjor Wuon Lat <127384804+Latjor-Wuon@users.noreply.github.com>
2023-10-06 16:58:11 +0000

    3-script.js

--- javascript-web_scraping/javascript-web_jquery/3-script.js
+++ javascript-web_scraping/javascript-web_jquery/3-script.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// a JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header
 $('div#red_header').click(function () {
-    $('header').addClass('red');
-  });
+	$('header').addClass('red');
+});
